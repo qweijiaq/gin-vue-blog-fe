5bdec443077fc442e4579390076062e73eebb1b7
weijia <2112025700@qq.com>
2023-12-30 16:29:35 +0800

    feat: 前端初始化项目

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { createApp } from "vue";
+import ArcoVue from "@arco-design/web-vue";
+import { createPinia } from "pinia";
+
+import App from "./App.vue";
+import router from "./router";
+
+import "@arco-design/web-vue/dist/arco.css";
+
+const app = createApp(App);
+
+app.use(ArcoVue);
+app.use(createPinia());
+app.use(router);
+
+app.mount("#app");
---
+++ src/stores/index.ts
@@ -0,0 +1,10 @@
+import { ref, computed } from "vue";
+import { defineStore } from "pinia";
+
+export const useStore = defineStore("store", {
+  state() {
+    return {};
+  },
+  actions: {},
+  getters: {},
+});
