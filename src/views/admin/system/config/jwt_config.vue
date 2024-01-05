7b9db9d8e27bb1647f9a679c3166687ec0a79372
weijia <2112025700@qq.com>
2024-01-05 23:33:11 +0800

    feat: 后台页面 —— 七牛云设置

--- src/views/admin/system/config/jwt_config.vue
+++ src/views/admin/system/config/jwt_config.vue
@@ -1,7 +1,46 @@
 <template>
-  <div>JWT配置</div>
+  <gvb-config
+    title="jwt配置"
+    name="jwt"
+    :left-width="isLaptops1 ? 40 : 30"
+    :right-with="isLaptops1 ? 60 : 70"
+    :column="column"
+    :help-data="helpList"
+  >
+  </gvb-config>
 </template>
+<script setup lang="ts">
+import GvbConfig from "@/components/admin/config.vue";
+import type { configColumnType } from "@/types";
+import type { jwtType } from "@/api/settings";
 
-<script setup lang="ts"></script>
+const isLaptops1 = (window as any).isLaptops;
 
-<style lang="scss"></style>
+const column: configColumnType<jwtType>[] = [
+  {
+    label: "秘钥",
+    field: "secret",
+    iptType: "password",
+    rules: [{ required: true, message: "请输入秘钥" }],
+  },
+  {
+    label: "颁发人",
+    field: "issuer",
+    rules: [{ required: true, message: "请输入颁发人" }],
+  },
+  { label: "过期时间", field: "expires", type: "number" },
+];
+
+const helpList = [
+  {
+    title: "秘钥",
+    abs: "用于jwt的加解密，请妥善保管",
+    content: "",
+  },
+  {
+    title: "过期时间",
+    abs: "单位小时",
+    content: "",
+  },
+];
+</script>
